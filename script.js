d80c579e5e62bbc449992b6d3d5e788638cc2fff
Marco Vasile <marcova996@gmail.com>
2024-04-17 10:52:14 +0200

    Get player choice

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const computerOption = ["rock", "paper", "scissors"];
 
 const computerChoice = getComputerChoice();
 
-console.log(computerChoice);
+let playerChoice = prompt("What will you choose? Rock, paper or scissors?");
+playerChoice = playerChoice.toLocaleLowerCase();
+
 
 function getComputerChoice() {
     const computerChoice = computerOption[Math.floor(Math.random() * computerOption.length)];
     return computerChoice;
 }
+
