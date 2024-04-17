0ab2b9b3127979db3bb3b4a358086cd75850483f
Marco Vasile <marcova996@gmail.com>
2024-04-17 10:28:30 +0200

    Create fucntion getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const computerOption = ["rock", "paper", "scissors"];
+
+const computerChoice = getComputerChoice();
+
+console.log(computerChoice);
+
+function getComputerChoice() {
+    const computerChoice = computerOption[Math.floor(Math.random() * computerOption.length)];
+    return computerChoice;
+}
