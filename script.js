3f14cf8657406cce336a70f51a45f4aa1ec4fe9b
Marco Vasile <marcova996@gmail.com>
2024-04-17 17:43:15 +0200

    Fix playRound, create function playGame

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const computerOption = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
-const computerChoice = getComputerChoice();
-console.log(computerChoice);
 
-let playerChoice = getPlayerChoice();
+playGame();
 
-let result = playRound(playerChoice, computerChoice);
+console.log(playerScore, computerScore);
 
-alert(result);
+
 
 
 
@@ -28,33 +28,58 @@ function getPlayerChoice()  {
     }
 }
 
-function playRound(playerChoice, computerChoice) {
+function playRound() {
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    let playerChoice = getPlayerChoice();
+
     if (playerChoice === "rock" && computerChoice === "scissors") {
         let result = "You win! Rock beats scissors!";
+        playerScore += 1;
         return result;
     } else if (playerChoice === "rock" && computerChoice === "paper") {
         let result = "You lose! Paper beats rock!";
+        computerScore += 1;
         return result;
     } else if (playerChoice === "rock" && computerChoice === "rock") {
         let result = "Draw!";
         return result;
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
         let result = "You lose! Scissors beats paper!";
+        computerScore += 1;
         return result;
     } else if (playerChoice === "paper" && computerChoice === "rock") {
         let result = "You win! Paper beats rock!";
+        playerScore += 1;
         return result;
     } else if (playerChoice === "paper" && computerChoice === "paper") {
         let result = "Draw!";
         return result;
     } else if (playerChoice === "scissors" && computerChoice === "paper") {
         let result = "You win! Scissors beat paper!";
+        playerScore += 1;
         return result;
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
         let result = "You lose! Rock beats scissors!";
+        computerScore += 1;
         return result;
     } else if (playerChoice === "scissors" && computerChoice === "scissors") {
         let result = "Draw!";
         return result;
+    }
+}
+
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        let result = playRound();
+        console.log(result);
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You win!")
+    } else if (playerScore === computerScore) {
+        console.log("Draw! Try playing again");
+    } else {
+        console.log("Oh no! You lose!");
     }
 }
