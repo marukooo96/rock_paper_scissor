ef9b80d3456e8db5a068a7586fd5522608cd8369
Marco Vasile <marcova996@gmail.com>
2024-04-17 11:26:22 +0200

    Create function getPlayerChoice

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const computerOption = ["rock", "paper", "scissors"];
 
 const computerChoice = getComputerChoice();
 
-let playerChoice = prompt("What will you choose? Rock, paper or scissors?");
-playerChoice = playerChoice.toLocaleLowerCase();
+let playerChoice = getPlayerChoice();
+
+console.log(playerChoice);
 
 
 function getComputerChoice() {
@@ -11,3 +12,14 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+function getPlayerChoice()  {
+    let playerChoice = prompt("What will you choose? Rock, paper or scissors?");
+    playerChoice = playerChoice.toLowerCase();  
+
+    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+        return playerChoice;
+    } else {
+        alert("You have to choose between rock, paper or scissors!");
+        return;
+    }
+}
